c8068ac70a96906e6842275f7b6de5d9c5db8bac
osapon <osa@osa-p.net>
2026-09-05 08:56:29 +0900

    add italiano

---
+++ Lang_it.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * 言語処理クラス (イタリア)
+ *
+ * @category Framework
+ * @package  Notestock
+ * @link     https://notestock.osa-p.net
+ */
+
+class Lang_it extends Lang
+{
+    public $langcode = 'it';
+    public $langname = "italiano";
+    public $menuname = 'italiano';
+    protected string $domain = 'it_IT';
+}
